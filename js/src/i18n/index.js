272003b6963b403d093959e306485143ab077a58
Krzysztof Sobczak <krzysztof@deum.eu>
2017-03-19 16:04:51 +0100

    polish translation

--- js/src/i18n/index.js
+++ js/src/i18n/index.js
@@ -1,9 +1,11 @@
 import en from './en';
+import pl from './pl';
 import fr from './fr';
 import zh from './zh';
 
 module.exports = {
   en,
+  pl,
   fr,
   zh,
 };
---
+++ js/src/i18n/pl.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+  // Generic
+  'generic.add': 'Dodaj',
+  'generic.cancel': 'Anuluj',
+
+  // BlockType
+  'components.controls.blocktype.h1': 'Nagłówek 1',
+  'components.controls.blocktype.h2': 'Nagłówek 2',
+  'components.controls.blocktype.h3': 'Nagłówek 3',
+  'components.controls.blocktype.h4': 'Nagłówek 4',
+  'components.controls.blocktype.h5': 'Nagłówek 5',
+  'components.controls.blocktype.h6': 'Nagłówek 6',
+  'components.controls.blocktype.blockquote': 'Cytat',
+  'components.controls.blocktype.blocktype': 'Typ elementu',
+  'components.controls.blocktype.normal': 'Normalny',
+
+  // Color Picker
+  'components.controls.colorpicker.text': 'Tekst',
+  'components.controls.colorpicker.background': 'Tło',
+
+  // Embedded
+  'components.controls.embedded.embeddedlink': 'Link do zewn. klipu',
+  'components.controls.embedded.enterlink': 'Wprowadź link',
+
+  // FontFamily
+  'components.controls.fontfamily.fontfamily': 'Czcionka',
+
+  // Image
+  'components.controls.image.fileUpload': 'Wgrywanie obrazu',
+  'components.controls.image.byURL': 'Adres URL',
+  'components.controls.image.dropFileText': 'Przeciągnij i upuść lub kliknij',
+
+  //Link
+  'components.controls.link.linkTitle': 'Nazwa odnośnika',
+  'components.controls.link.linkTarget': 'Adres WWW',
+}
